38935836a07c99cebb7c9b91eec4451d21a03b5e
MatheusTrein <matheusptrein@hotmail.com>
2023-04-19 16:50:22 -0300

    Prepara página do jogo

---
+++ src/pages/Game.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>Game</h1>
+      </>
+    );
+  }
+}
+
+export default Game;
