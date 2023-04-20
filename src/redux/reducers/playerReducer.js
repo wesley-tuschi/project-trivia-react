447f7cc526b3fe862ff0452faf8ab90cfbbb477e
MatheusTrein <matheusptrein@hotmail.com>
2023-04-20 13:33:41 -0300

    Refatorando

--- src/redux/reducers/playerReducer.js
+++ src/redux/reducers/playerReducer.js
@@ -9,8 +9,6 @@ const INITIAL_STATE = {
 
 const playerReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case 'SAVE_INFO':
-    return state;
   case LOG_IN:
     return {
       ...state,
