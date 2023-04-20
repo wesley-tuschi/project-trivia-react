d20dab07585d3e83f437f65013d16290de71140e
Wesley Tuschi <levituschi@gmail.com>
2023-04-20 15:39:14 -0300

    requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -8,31 +8,39 @@ class Feedback extends Component {
     const message0to3 = 'Could be better...';
     const message3OrMore = 'Well Done!';
     const three = 3;
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
 
     return (
-
-      <div>
+      <>
         <Header />
-        {
-          assertions <= { three } ? (
-            <p data-testid="feedback-text">
-              { message0to3 }
-            </p>
-          )
-            : <p data-testid="feedback-text">{message3OrMore }</p>
-        }
-      </div>
+        <div>
+          {
+            assertions <= { three } ? (
+              <p data-testid="feedback-text">
+                { message0to3 }
+              </p>
+            )
+              : <p data-testid="feedback-text">{message3OrMore }</p>
+          }
+        </div>
+        <div>
+          <p data-testid="feedback-total-score">{score}</p>
+          <p data-testid="feedback-total-question">{assertions}</p>
+        </div>
+      </>
     );
   }
 }
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
